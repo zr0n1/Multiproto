06e72627d07184134a56aec1848bc6d263964b3d
0n1 <zr0n1@proton.me>
2024-07-03 15:06:22 -0400

    added version property to VersionChangedEvent

--- src/main/java/com/github/zr0n1/multiproto/event/VersionChangedEvent.kt
+++ src/main/java/com/github/zr0n1/multiproto/event/VersionChangedEvent.kt
@@ -1,5 +1,8 @@
 package com.github.zr0n1.multiproto.event
 
+import com.github.zr0n1.multiproto.protocol.Protocol
 import net.mine_diver.unsafeevents.Event
 
-class VersionChangedEvent : Event()
+class VersionChangedEvent : Event() {
+    val version = Protocol.version
+}
